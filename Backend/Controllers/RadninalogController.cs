c5f2535047bd01103259d9df34297f9b03e482e6
Krešimir <ilead7@gmail.com>
2024-04-16 18:20:27 +0200

    Put za radni nalog

--- Backend/Controllers/RadninalogController.cs
+++ Backend/Controllers/RadninalogController.cs
@@ -9,7 +9,7 @@ namespace Backend.Controllers
 {
     [ApiController]
     [Route("api/v1/[controller]")]
-    public class  RadninalogController : ReklamacijskiController<Radninalog,RadninalogDTORead,RadninalogDTOInsertUpdate>
+    public class RadninalogController : ReklamacijskiController<Radninalog, RadninalogDTORead, RadninalogDTOInsertUpdate>
     {
         public RadninalogController(ReklamacijskiPultContext context) : base(context)
         {
@@ -18,6 +18,28 @@ namespace Backend.Controllers
         }
         protected override void KontrolaBrisanje(Radninalog entitet)
         {
-           if (entitet != null && entitet.) { }
+            if (entitet != null && entitet.Kupac != null)
+            {
+                throw new Exception("Ne može se obrisati radni nalog jer ima kupca");
+            }
+
+        }
+
+        protected override Radninalog KreirajEntitet(RadninalogDTOInsertUpdate dto)
+        {
+            var kupac = _context.Kupci.Find(dto.KupacSifra) ?? throw new Exception("Ne postoji kupac sa šifrom :"+dto.KupacSifra+"u bazi");
+            var proizvod = _context.Proizvodi.Find(dto.ProizvodSifra) ?? throw new Exception("Ne postoji proizvod sa šifrom" + dto.ProizvodSifra + "u bazi");
+            var entitet = _mapper.MapInsertUpdatedFromDTO(dto);
+            var napomena = dto.Napomena;
+
+            entitet.Proizvod = proizvod;
+            entitet.Kupac = kupac;
+            entitet.Datum = DateTime.Now;
+            entitet.Napomena = napomena;
+            return entitet;
+            
+        }
+        
+
     }
 }
